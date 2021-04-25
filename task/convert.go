c76af2f21f5da58866ffa2725b2a1f6a587d528d
errolying <errolying@tencent.com>
2021-04-25 10:39:22 +0800

    fix value = nil

--- task/convert.go
+++ task/convert.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"errors"
 	"gopkg.in/yaml.v2"
 	"reflect"
 	"strings"
@@ -17,20 +18,31 @@ func Convert(yamlCode string, indent string) string {
 
 	//fmt.Printf("%v", t)
 
-	result := formatMap(t, 0, indent, "Config")
+	result, err := formatMap(t, 0, indent, "Config")
+
+	if err != nil {
+		return err.Error()
+	}
 	//println(result)
 	return result
 }
 
-func formatMap(m map[string]interface{}, index int, indent string, outName string) string {
+func formatMap(m map[string]interface{}, index int, indent string, outName string) (string, error) {
 	result := make([]string, 0)
 
 	result = append(result, getHeader(index, indent, outName))
 
 	for key, value := range m {
+		if reflect.TypeOf(value) == nil {
+			return "", errors.New(key + ": key without value")
+		}
 		if reflect.TypeOf(value).Kind() == reflect.Map {
 			newValue := getMap(value.(map[interface{}]interface{}))
-			result = append(result, formatMap(newValue, index+1, indent, key))
+			next, err := formatMap(newValue, index+1, indent, key)
+			if err != nil {
+				return "", errors.New(key + "." + err.Error())
+			}
+			result = append(result, next)
 		} else if reflect.TypeOf(value).Kind() == reflect.Array {
 			result = append(result, getArrayBody(index, indent, key, value.([]interface{})))
 		} else {
@@ -42,7 +54,7 @@ func formatMap(m map[string]interface{}, index int, indent string, outName strin
 
 	resultString := strings.Join(result, "\n")
 
-	return resultString
+	return resultString, nil
 }
 
 func getMap(m map[interface{}]interface{}) map[string]interface{} {
